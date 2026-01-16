26850a1abad7483f780a65d77bc67d0d23037553
Shu <yeoshuyi123@gmail.com>
2026-01-16 14:22:00 +0800

    Gen 100 and trim method

--- T2Q3.py
+++ T2Q3.py
@@ -1,16 +1,12 @@
 """
 Tutorial 2, Question 3.
 
-Generate random numbers until their sum hits 1000.
-Partially vectorized solution to still use for loops taught in lesson.
-First 50 numbers are immediately generated without going through sum<1000 check.
-
-Alternative solution to generate 1000 numbers and trim to sum<1000.
-Might be faster as vector operations are optimized in np.
+Generate 100 and trim to avoid long while loop.
+This reduces overhead compared to a generate and check loop.
+Where N = 100 elements, time and space complexity is O(N).
 """
 
-
-from numpy import random as rd
+import numpy as np
 import time
 
 
@@ -22,30 +18,36 @@ class RandomNumGen:
         self.rand_sum = 0
 
         #Standardising seed for reproducability
-        rd.seed(0) if __debug__ else rd.seed(int(time.time()))
+        np.random.seed(0) if __debug__ else np.random.seed(int(time.time()))
     
     def generate_list(self):
-        """Iterate until sum hits 1000"""
+        """Generate list of 100 ints 1-20 and trim"""
 
-        #Generate 50 numbers immediately without checking sum <1000
-        #Slightly reduces overhead
-        batch_write = rd.randint(1, 21, size=50)
-        self.rand_list.extend(batch_write.tolist())
-        self.rand_sum = batch_write.sum()
+        #Generate 100 numbers immediately without checking sum <1000
+        self.rand_list.extend(np.random.randint(1, 21, size=100).tolist())
+        self.rand_sum = np.sum(self.rand_list)
 
-        while self.rand_sum < 1000:
-            rand_num = rd.randint(1, 21)
-            self.rand_list.append(rand_num)
-            self.rand_sum += rand_num
+        if self.rand_sum <= 1000:
+            return self.rand_list
+
+        while self.rand_sum > 999:
+            last_item = self.rand_list[-1]
+            self.rand_list.pop()
+            self.rand_sum -= last_item
         
-        return 0
+        self.rand_list.append(last_item)
+        self.rand_sum += last_item
+        
+        return self.rand_list
 
 
 if __name__ == "__main__":
+    current_time = time.time()
     rand = RandomNumGen()
     rand.generate_list()
     print(
-        f"Random numbers summed to: {rand.rand_sum}\n"
-        f"List contains: {rand.rand_list}"
+        f"Random numbers summed to {rand.rand_sum} with {len(rand.rand_list)} elements.\n"
+        f"List contains: {rand.rand_list}\n"
+        f"Generation took {(time.time()-current_time):.4f}s."
     )
         
