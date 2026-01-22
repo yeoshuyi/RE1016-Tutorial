bac3fc846ca05d364951b6f6f0ae3a85a852ca19
Shu <yeoshuyi123@gmail.com>
2026-01-22 12:42:38 +0800

    Added option to T2Q3

--- T2Q3.py
+++ T2Q3.py
@@ -20,10 +20,43 @@ class RandomNumGen:
         #Standardising seed for reproducability
         np.random.seed(0) if __debug__ else np.random.seed(int(time.time()))
     
-    def generate_list(self):
+    def list_reset(self):
+        """Resets all list to blank"""
+
+        self.rand_list = []
+        self.rand_sum = 0
+
+    def generate_list_bruteforce(self):
+        """Generate ints 1-20 until either sum > 1000 or 100 ints generated"""
+
+        self.list_reset()
+        count = 0
+        while self.rand_sum < 1000 and count < 100:
+            generate_random = np.random.randint(1, 21)
+            self.rand_list.append(generate_random)
+            self.rand_sum += generate_random
+            count += 1
+
+    def generate_list_minbatch(self):
+        """Generate list of 50 ints 1-20 and add"""
+
+        #Generate 50 numbers immediately, guranteed to not exceed 1000
+        self.list_reset()
+        self.rand_list.extend(np.random.randint(1, 21, size=50).tolist())
+        self.rand_sum = np.sum(self.rand_list)
+        count = 0
+
+        while self.rand_sum < 1000 and count < 50:
+            generate_random = np.random.randint(1, 21)
+            self.rand_list.append(generate_random)
+            self.rand_sum += generate_random
+            count += 1
+
+    def generate_list_maxbatch(self):
         """Generate list of 100 ints 1-20 and trim"""
 
         #Generate 100 numbers immediately without checking sum <1000
+        self.list_reset()
         self.rand_list.extend(np.random.randint(1, 21, size=100).tolist())
         self.rand_sum = np.sum(self.rand_list)
 
@@ -41,13 +74,45 @@ class RandomNumGen:
         return self.rand_list
 
 
-if __name__ == "__main__":
-    current_time = time.time()
+def main():
+    """Get user input on generation type and print results"""
+
+    while True:
+        try:
+            option = input(
+                f"Please select from the following generation options:\n"
+                f"1. Bruteforce Method\n"
+                f"2. Minbatch Method\n"
+                f"3. Maxbatch Method\n"
+                f"Select: "
+            )
+            option = int(option)
+            if 0 < option < 4:
+                break
+            else:
+                print("Please select 1-3.")
+                continue
+        except ValueError:
+            print("Wrong input type.")
+
     rand = RandomNumGen()
-    rand.generate_list()
+    match option:
+        case 1:
+            current_time = time.time()
+            rand.generate_list_bruteforce()
+        case 2:
+            current_time = time.time()
+            rand.generate_list_minbatch()
+        case 3:
+            current_time = time.time()
+            rand.generate_list_maxbatch()
+    
     print(
         f"Random numbers summed to {rand.rand_sum} with {len(rand.rand_list)} elements.\n"
         f"List contains: {rand.rand_list}\n"
-        f"Generation took {(time.time()-current_time):.4f}s."
+        f"Generation took {(time.time()-current_time) * 1000 * 1000:.3f}us."
     )
-        
+
+
+if __name__ == "__main__":
+    main()
