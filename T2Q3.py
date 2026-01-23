9c3171c7212e9a58083c18ac94c4173491fa888c
Shu <yeoshuyi123@gmail.com>
2026-01-23 12:05:59 +0800

    Generalised to all

--- T2Q3.py
+++ T2Q3.py
@@ -1,16 +1,22 @@
 """
 Tutorial 2, Question 3.
 
-Generate up to 100 random integers that sum up to 1000.
-Each integer ranges 1 - 20.
+Generate up to MAX_LIST_SIZE random integers that sum up to MAX_LIST_SUM.
+Each integer ranges MIN_RANDOM - MAX_RANDOM.
 
-3 methods of generation is proposed, using:
+4 methods of generation is proposed, using:
 a) Bruteforce, b) Min Batch, c) Max Batch, d) Max Batch with Cumulative Sum
 """
 
 
 import numpy as np
 import time
+
+
+MAX_LIST_SIZE = 100
+MAX_LIST_SUM = 1000
+MIN_RANDOM = 1
+MAX_RANDOM = 20
 
 
 class RandomNumGen:
@@ -25,7 +31,7 @@ class RandomNumGen:
         self.rand_list = []
         self.rand_sum = 0
 
-        #Standardising seed for reproducability
+        #Standardising seed for reproducability when debugging
         np.random.seed(0) if __debug__ else np.random.seed(int(time.time()))
     
     def list_reset(self):
@@ -36,15 +42,16 @@ class RandomNumGen:
 
     def generate_list_bruteforce(self):
         """
-        Generate a list of I until either the sum reaches 1000, or len(list) reaches 100,
-        where I is an integer ranging from 1-20.\n
-        Bruteforce method appends one integer and checks sum with ever iteration.
+        Generate a list of I until either the sum reaches MAX_LIST_SUM, or len(list) reaches MAX_LIST_SIZE,
+        where I is an integer ranging from MIN_RANDOM to MAX_RANDOM.\n
+        Bruteforce method appends one integer and checks sum with ever iteration.\n
+        Returns list by defualt.
         """
 
         self.list_reset()
         count = 0
-        while self.rand_sum < 1000 and count < 100:
-            generate_random = np.random.randint(1, 21)
+        while self.rand_sum < MAX_LIST_SUM and count < MAX_LIST_SIZE:
+            generate_random = np.random.randint(MIN_RANDOM, MAX_RANDOM+1)
             self.rand_list.append(generate_random)
             self.rand_sum += generate_random
             count += 1
@@ -52,19 +59,22 @@ class RandomNumGen:
 
     def generate_list_minbatch(self):
         """
-        Generate a list of I until either the sum reaches 1000, or len(list) reaches 100,
-        where I is an integer ranging from 1-20.\n
-        Minbatch method generates 50 integers immediately, and appends with each iteration.
+        Generate a list of I until either the sum reaches MAX_LIST_SUM, or len(list) reaches MAX_LIST_SIZE,
+        where I is an integer ranging from MIN_RANDOM to MAX_RANDOM.\n
+        initial_size is the maximum size such that list is GURANTEED to be below MAX_LIST_SUM.\n
+        Minbatch method generates initial_size integers immediately, and appends with each iteration.\n
+        Returns list by defualt.
         """
 
         #Generate 50 numbers immediately, guranteed to not exceed 1000
         self.list_reset()
-        self.rand_list.extend(np.random.randint(1, 21, size=50).tolist())
+        initial_size = min(MAX_LIST_SUM // MAX_RANDOM, MAX_LIST_SIZE)
+        self.rand_list.extend(np.random.randint(MIN_RANDOM, MAX_RANDOM+1, size=initial_size).tolist())
         self.rand_sum = np.sum(self.rand_list)
-        count = 0
+        count = initial_size
 
-        while self.rand_sum < 1000 and count < 50:
-            generate_random = np.random.randint(1, 21)
+        while self.rand_sum < MAX_LIST_SUM and count < MAX_LIST_SIZE:
+            generate_random = np.random.randint(MIN_RANDOM, MAX_RANDOM+1)
             self.rand_list.append(generate_random)
             self.rand_sum += generate_random
             count += 1
@@ -72,20 +82,21 @@ class RandomNumGen:
 
     def generate_list_maxbatch(self):
         """
-        Generate a list of I until either the sum reaches 1000, or len(list) reaches 100,
-        where I is an integer ranging from 1-20.\n
-        Maxbatch method generates 100 integers immediately, and trims with each iteration.
+        Generate a list of I until either the sum reaches MAX_LIST_SUM, or len(list) reaches MAX_LIST_SIZE,
+        where I is an integer ranging from MIN_RANDOM to MAX_RANDOM.\n
+        Maxbatch method generates MAX_LIST_SIZE integers immediately, and trims with each iteration.\n
+        Returns list by defualt.
         """
 
         #Generate 100 numbers immediately without checking sum <1000
         self.list_reset()
-        self.rand_list.extend(np.random.randint(1, 21, size=100).tolist())
+        self.rand_list.extend(np.random.randint(MIN_RANDOM, MAX_RANDOM+1, size=MAX_LIST_SIZE).tolist())
         self.rand_sum = np.sum(self.rand_list)
 
-        if self.rand_sum <= 1000:
+        if self.rand_sum <= MAX_LIST_SUM:
             return self.rand_list
 
-        while self.rand_sum > 999:
+        while self.rand_sum > MAX_LIST_SUM-1:
             last_item = self.rand_list[-1]
             self.rand_list.pop()
             self.rand_sum -= last_item
@@ -96,23 +107,24 @@ class RandomNumGen:
     
     def generate_list_maxbatch_vec(self):
         """
-        Generate a list of I until either the sum reaches 1000, or len(list) reaches 100,
-        where I is an integer ranging from 1-20.\n
-        Maxbatch method generates 100 integers immediately, and trims using cumulative sum.
+        Generate a list of I until either the sum reaches MAX_LIST_SUM, or len(list) reaches MAX_LIST_SIZE,
+        where I is an integer ranging from MIN_RANDOM to MAX_RANDOM.\n
+        Maxbatch method generates MAX_LIST_SIZE integers immediately, and trims using cumulative sum.\n
+        Returns list by defualt.
         """
 
         #Generate 100 numbers immediately without checking sum <1000
         self.list_reset()
-        temporary_list = (np.random.randint(1, 21, size=100).tolist())
+        temporary_list = (np.random.randint(MIN_RANDOM, MAX_RANDOM+1, size=MAX_LIST_SIZE).tolist())
         self.rand_sum = np.sum(temporary_list)
-        if self.rand_sum <= 1000:
+        if self.rand_sum <= MAX_LIST_SUM:
             self.rand_list.extend(temporary_list)
             return self.rand_list
     
         cumulative_sum = np.cumsum(temporary_list)
 
         #Find index where cumulative sum first exceeds 1000
-        index = np.searchsorted(cumulative_sum, 999, "right") + 1
+        index = np.searchsorted(cumulative_sum, MAX_LIST_SUM-1, "right") + 1
         self.rand_list.extend(temporary_list[:index])
         self.rand_sum = np.sum(self.rand_list)
         return self.rand_list
@@ -136,10 +148,17 @@ def main():
             if 0 < option < 5:
                 break
             else:
-                print("Please select 1-4.")
+                print(f"\n-------------------------------------------------------------\n"
+                      f"Please select 1-4.\n"
+                      f"-------------------------------------------------------------\n"
+                )
                 continue
         except ValueError:
-            print("Wrong input type.")
+            print(
+                f"\n-------------------------------------------------------------\n"
+                f"Wrong input type.\n"
+                f"-------------------------------------------------------------\n"
+            )
 
     rand = RandomNumGen()
     match option:
@@ -158,9 +177,10 @@ def main():
     
     print(
         f"-------------------------------------------------------------\n"
-        f"Random numbers summed to {rand.rand_sum} with {len(rand.rand_list)} elements.\n"
         f"List contains: {rand.rand_list}\n"
-        f"Generation took {(time.time()-current_time) * 1000 * 1000:.3f}us."
+        f"Random numbers summed to {rand.rand_sum} with {len(rand.rand_list)} elements.\n"
+        f"Generation took {(time.time()-current_time) * 1000 * 1000:.3f}us.\n"
+        f"-------------------------------------------------------------"
     )
 
 
